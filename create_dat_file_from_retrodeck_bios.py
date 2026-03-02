9c0acdc4dc5b611ee0a762fecf81df93bfc4389c
MForrest00 <matthew_forrest@ymail.com>
2026-03-01 19:49:21 -0600

    Update create_dat_file_from_retrodeck_bios.py

--- create_dat_file_from_retrodeck_bios.py
+++ create_dat_file_from_retrodeck_bios.py
@@ -57,58 +57,60 @@ def main():
     root = create_starter_xml_tree()
 
     for i in COMPONENTS_REPO_PATH.iterdir():
-        if i.is_dir():
-            for j in i.iterdir():
-                if j.is_file() and j.name == "component_manifest.json":
-                    with open(j, "r") as f:
-                        data = json.load(f)
+        if not i.is_dir():
+            continue
+        for j in i.iterdir():
+            if not j.is_file() or j.name != "component_manifest.json":
+                continue
+            with open(j, "r") as f:
+                data = json.load(f)
 
-                    bios = recurse_for_bios(data, i.name)
-                    for b in bios:
-                        system = b["system"]
-                        if not isinstance(system, list):
-                            system = [system]
-                        for s in system:
-                            md5 = b.get("md5")
-                            multiple_md5s = True
-                            if not md5:
-                                md5 = [None]
-                            elif not isinstance(md5, list):
-                                multiple_md5s = False
-                                md5 = [md5]
+            bios = recurse_for_bios(data, i.name)
+            for b in bios:
+                system = b["system"]
+                if not isinstance(system, list):
+                    system = [system]
+                for s in system:
+                    md5 = b.get("md5")
+                    multiple_md5s = True
+                    if not md5:
+                        md5 = [None]
+                    elif not isinstance(md5, list):
+                        multiple_md5s = False
+                        md5 = [md5]
 
-                            for m in md5:
-                                emulator_system_key = f"{i.name} - {s}"
-                                name = b["filename"]
-                                if m is None:
-                                    emulator_system_key += " (no hashes)"
-                                elif multiple_md5s:
-                                    emulator_system_key += f' ({"".join(x for x in name if x.isalnum())} - {m})'
+                    for m in md5:
+                        emulator_system_key = f"{i.name} - {s}"
+                        name = b["filename"]
+                        if m is None:
+                            emulator_system_key += " (no hashes)"
+                        elif multiple_md5s:
+                            emulator_system_key += f' ({"".join(x for x in name if x.isalnum())} - {m})'
 
-                                game = sub_elements_component_system_dict.get(emulator_system_key)
-                                if game is None:
-                                    game = SubElement(root, "game")
-                                    game.set("name", emulator_system_key)
-                                    sub_elements_component_system_dict[emulator_system_key] = game
+                        game = sub_elements_component_system_dict.get(emulator_system_key)
+                        if game is None:
+                            game = SubElement(root, "game")
+                            game.set("name", emulator_system_key)
+                            sub_elements_component_system_dict[emulator_system_key] = game
 
-                                for k in b.keys():
-                                    if k not in KNOWN_BIOS_KEYS:
-                                        raise Exception(f"Unknown BIOS key encountered: '{k}' in component {i.name}")
-                                rom = SubElement(game, "rom")
-                                rom.set("name", name)
-                                if m is not None:
-                                    rom.set("md5", m)
-                                rom.set("description", b["description"])
-                                if "required" in b:
-                                    rom.set("required", b["required"])
-                                if "sha256" in b:
-                                    rom.set("sha256", b["sha256"])
-                                paths = b.get("paths", [])
-                                if isinstance(paths, str):
-                                    paths = [paths]
-                                for p in paths:
-                                    path = SubElement(rom, "path")
-                                    path.text = p
+                        for k in b.keys():
+                            if k not in KNOWN_BIOS_KEYS:
+                                raise Exception(f"Unknown BIOS key encountered: '{k}' in component {i.name}")
+                        rom = SubElement(game, "rom")
+                        rom.set("name", name)
+                        if m is not None:
+                            rom.set("md5", m)
+                        rom.set("description", b["description"])
+                        if "required" in b:
+                            rom.set("required", b["required"])
+                        if "sha256" in b:
+                            rom.set("sha256", b["sha256"])
+                        paths = b.get("paths", [])
+                        if isinstance(paths, str):
+                            paths = [paths]
+                        for p in paths:
+                            path = SubElement(rom, "path")
+                            path.text = p
 
     with open(f"Retrodeck BIOS {DAT_VERSION_NUMBER}.dat", "wb") as f:
         f.write(b'<?xml version="1.0" encoding="UTF-8"?>\n')
